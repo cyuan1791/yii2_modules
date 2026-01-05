20b31546db41b22c86f8725f6dcf825377b23813
Mark Yuan <mark@webasone.com>
2026-01-05 20:07:41 +0000

    a

---
+++ rate/main_sample.php
@@ -0,0 +1,13 @@
+<?php 
+
+// add this to the main.php
+
+
+return [
+	 
+  'modules' => [
+            'rate' => [
+                    'class' => 'frontend\modules\rate\module',
+            ],
+    ],
+];
